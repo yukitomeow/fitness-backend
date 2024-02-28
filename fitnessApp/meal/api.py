b63f79945898b5b24578875c6fc98e009fd3a431
Yukimi Otagiri <Yukimi.otagiri@gmail.com>
2024-02-28 20:37:09 +0900

    Added is authenticated function on meal/api.py

--- fitnessApp/meal/api.py
+++ fitnessApp/meal/api.py
@@ -2,28 +2,36 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Meal, FoodItem, Nutrition
 from .serializers import MealSerializer, FoodItemSerializer, NutritionSerializer
+from rest_framework.permissions import IsAuthenticated
+
+
 # This was views.py Instead of view set
 
 class MealListCreateView(ListCreateAPIView):
     serializer_class = MealSerializer
     queryset = Meal.objects.all()
-
+    permission_classes = [IsAuthenticated]
 class MealDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class = MealSerializer
     queryset = Meal.objects.all()
+    permission_classes = [IsAuthenticated]
 
 class FoodItemListCreateView(ListCreateAPIView):
     serializer_class = FoodItemSerializer
     queryset = FoodItem.objects.all()
+    permission_classes = [IsAuthenticated]
 
 class FoodItemDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class = FoodItemSerializer
     queryset = FoodItem.objects.all()
+    permission_classes = [IsAuthenticated]
 
 class NutritionListCreateView(ListCreateAPIView):
     serializer_class = NutritionSerializer
     queryset = Nutrition.objects.all()
+    permission_classes = [IsAuthenticated]
 
 class NutritionDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class = NutritionSerializer
     queryset = Nutrition.objects.all()
+    permission_classes = [IsAuthenticated]
